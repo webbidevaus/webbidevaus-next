9cd02224a6d3f7c6919e4c94eda12f959b160fd2
Riku Rouvila <riku.rouvila@gmail.com>
2021-07-04 13:34:59 +0300

    turn all episode descriptions to markdown

--- generate-episodes-json.ts
+++ generate-episodes-json.ts
@@ -1,9 +1,34 @@
 import { writeFileSync } from "fs";
-import { getEpisodes } from "./util/episodes";
+import { Episode } from "./types/Episode";
+import { getEpisode, getEpisodes } from "./util/episodes";
+import { GetInterface } from "purify-ts";
+import TurndownService from "turndown";
 
 async function main() {
   const episodes = await getEpisodes(process.env.SIMPLECAST_PODCAST_ID);
-  writeFileSync("./public/episodes.json", JSON.stringify(episodes.collection));
+  writeFileSync("episodes.json.tmp", JSON.stringify(episodes));
+
+  let fullEpisodeDetails: Array<GetInterface<typeof Episode>> = [];
+
+  for (const episode of episodes.collection) {
+    const mEpisode = await getEpisode(
+      "episodes.json.tmp",
+      episode.number.toString()
+    );
+
+    fullEpisodeDetails = mEpisode.caseOf({
+      Just: (e) => {
+        // Episodes before 55 have description in markdown, others are in HTML
+        const episodeWithDescriptionAsHTML = {
+          ...e,
+          long_description: new TurndownService().turndown(e.long_description),
+        };
+        return fullEpisodeDetails.concat(episodeWithDescriptionAsHTML);
+      },
+      Nothing: () => fullEpisodeDetails,
+    });
+  }
+  writeFileSync("public/episodes.json", JSON.stringify(fullEpisodeDetails));
 }
 
 main();
